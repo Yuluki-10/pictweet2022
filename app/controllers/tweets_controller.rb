f4d3dd0518f8ae433310c07e1f971ec85cb478da
Yuluki <web.yulukitaiga@gmail.com>
2022-04-20 12:42:12 +0900

    save categories controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,25 @@
 class TweetsController < ApplicationController
-
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
   def index
-    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    @tweets= Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    @category_list = Category.all
   end
 
   def new
   end
 
   def create
-    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    # 選択から選んだカテゴリー
+    if params[:category_ids]
+      collection_category_list = Category.where(id: params[:category_ids]).map(&:category_name)
+      category_list = collection_category_list + params[:category_names].split(",")
+    else
+      category_list = params[:category_names].split(",")
+    end
+    # category_list = tweet_params[:category_names].split(",")
+    category_list_uniq = category_list.uniq
+    @tweet = Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    @tweet.save_category(category_list_uniq)
     # binding.pry
   end
 
@@ -17,15 +27,25 @@ class TweetsController < ApplicationController
     tweet = Tweet.find(params[:id])
     tweet.destroy if tweet.user_id == current_user.id
   end
-  
+
   def edit
     @tweet = Tweet.find(params[:id])
+    @category_list = @tweet.categories.pluck(:category_name).join(',')
   end
 
   def update
     tweet = Tweet.find(params[:id])
+    if params[:category_ids]
+      collection_category_list = Category.where(id: params[:category_ids]).map(&:category_name)
+      category_list = collection_category_list + params[:category_names].split(",")
+    else
+      category_list = params[:category_names].split(",")
+    end
+    category_list_uniq = category_list.uniq
     if tweet.user_id == current_user.id
-      tweet.update(tweet_params)
+      #こうしないと⬇︎、categoryに関するパラメータをpermitしてくれない
+      tweet.update(image: tweet_params[:image], text: tweet_params[:text])
+      tweet.save_category(category_list_uniq)
     end
   end
 
@@ -34,12 +54,18 @@ class TweetsController < ApplicationController
     @comments = @tweet.comments.includes(:user)
   end
 
+  def search
+    @category_list = Category.all
+    @category = Category.find(params[:category_id])
+    @tweets = @category.tweets.all.page(params[:page]).per(5).order("created_at DESC")
+  end
+
   private
     def tweet_params
       params.permit(:image, :text)
     end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to action: :index unless user_signed_in? #user_signed_inメソッドは、deviseのメソッド
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,19 @@ class Tweet < ApplicationRecord
   has_many :likes
   has_many :category_maps, dependent: :destroy
   has_many :categories, through: :category_maps
+
+  def save_category(sent_categories)
+    current_categories = self.categories.pluck(:category_name) unless self.categories.nil?
+    old_categories = current_categories - sent_categories
+    new_categories = sent_categories - current_categories
+
+    old_categories.each do |old|
+      self.categories.delete(Category.find_by(category_name: old))
+    end
+
+    new_categories.each do |new|
+      new_tweet_category = Category.find_or_create_by(category_name: new)
+      self.categories << new_tweet_category
+    end
+  end
 end
